2ffabeca663a3267bdde57d8e15eb7d22c387a07
Monty-TheCoder <126876913+Monty-TheCoder@users.noreply.github.com>
2024-03-19 11:25:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://tictoctoebyskp.netlify.app/"
+        origin: "https://tictoctoebyskp.netlify.app"
     },
 });
 let arr = new Array(9).fill('')
